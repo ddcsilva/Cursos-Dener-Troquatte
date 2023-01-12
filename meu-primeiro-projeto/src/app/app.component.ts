d63619dc8d030cb3b3069ab4694de3756fadbb1d
Danilo Silva <danilo.silva@msn.com>
2023-01-12 20:17:21 -0300

    Aula "ng-content"

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-diretivas-atributos></app-diretivas-atributos>
+    <app-diretivas-atributos>
+      <h1>Aulas de Diretivas Atributo</h1>
+      <h3>Final da aula</h3>
+    </app-diretivas-atributos>
     <router-outlet></router-outlet>
   `,
 })
