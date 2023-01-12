1379d0b229d9eb3e08ee450c130d3f495294ac89
Danilo Silva <danilo.silva@msn.com>
2023-01-12 20:20:47 -0300

    Aula "Pipes"

--- meu-primeiro-projeto/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ meu-primeiro-projeto/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -10,6 +10,7 @@ export class DiretivasAtributosComponent implements OnInit {
   public altura: string = '20px';
   public nome: string = '';
   public lista: Array<{ nome: string }> = [];
+  public date: Date = new Date();
 
   constructor() {}
 
