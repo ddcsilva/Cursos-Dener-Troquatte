bd40f577acfee1f2f1d8b19ae850e8e17598f832
Danilo Silva <danilo.silva@msn.com>
2023-01-12 16:01:52 -0300

    Aula "Two-way Binding"

--- meu-primeiro-projeto/src/app/app.module.ts
+++ meu-primeiro-projeto/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TitleComponent,
-    DataBindingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TitleComponent, DataBindingComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
