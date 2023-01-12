1ee7176635a1eac84ee35739d01128cea1117fb2
Danilo Silva <danilo.silva@msn.com>
2023-01-12 15:55:40 -0300

    Aula "Event Binding"

--- meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -15,7 +15,19 @@ export class DataBindingComponent implements OnInit {
     'https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
   public imgTitle: string = 'Imagem de Teste';
 
+  public position: { x: number; y: number } = { x: 0, y: 0 };
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMoveTest(valor: MouseEvent) {
+    console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
