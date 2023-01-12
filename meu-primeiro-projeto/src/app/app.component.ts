5607b8cf1157739148a7de11de0c3fcd3fdf8786
Danilo Silva <danilo.silva@msn.com>
2023-01-12 20:43:03 -0300

    Aula "Criando Sub-modules"

--- meu-primeiro-projeto/src/app/app.component.ts
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -3,10 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-diretivas-atributos>
-      <h1>Aulas de Diretivas Atributo</h1>
-      <h3>Final da aula</h3>
-    </app-diretivas-atributos>
+    <app-new-component></app-new-component>
     <router-outlet></router-outlet>
   `,
 })
