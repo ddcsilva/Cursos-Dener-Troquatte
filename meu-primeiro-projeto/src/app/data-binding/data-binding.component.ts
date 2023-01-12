62b800523c361ba8c6d431c75bfc4e1dfe860050
Danilo Silva <danilo.silva@msn.com>
2023-01-12 15:43:27 -0300

    Aula "Property Binding"

--- meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
+++ meu-primeiro-projeto/src/app/data-binding/data-binding.component.ts
@@ -10,6 +10,11 @@ export class DataBindingComponent implements OnInit {
   public idade: number = 33;
   public maisUm: number = 1;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc: string =
+    'https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+  public imgTitle: string = 'Imagem de Teste';
+
   constructor() {}
 
   ngOnInit(): void {}
